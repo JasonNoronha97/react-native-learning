d212b3493230fa43975dffbbd5112f4f037e141f
Jason Noronha <jason@smarthomesentry.com>
2020-11-12 12:02:34 +0530

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Opened up App.tsx and started working on my app!</Text>
     </View>
   );
 }
